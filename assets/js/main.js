bde589fd8e5e87f57cb0cade32e49e0db73ca8f1
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-14 14:47:17 +0200

    create v-for function for all the contacts

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            message : 'ciao',
             contacts: [
                 {
                     name: 'Michele',
